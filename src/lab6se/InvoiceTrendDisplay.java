19a158bb64ba7ee57d8f347a93f9c2b20bb437fd
1301147 <1301147@RG43008.rgu.ac.uk>
2015-03-23 09:24:44 +0000

    Invoices and Observers

--- src/lab6se/InvoiceTrendDisplay.java
+++ src/lab6se/InvoiceTrendDisplay.java
@@ -5,10 +5,33 @@
  */
 package lab6se;
 
+import java.util.Date;
+
 /**
  *
  * @author 1301147
  */
-public class InvoiceTrendDisplay {
+public class InvoiceTrendDisplay  implements Observer{
+
+    double currentInvoiceAmount;
+    double lastInvoiceAmount;
+    double previousInvoiceAmount;
+    double trendAmount;
+    Date trendDate;
+    String trendRecipient;
+    private Subject trendData;
+    
+    @Override
+    public void update(double newTrendAmount, Date newTrendDate, String newTrendRecipient) {
+       this.trendAmount = newTrendAmount;
+        this.trendDate = newTrendDate;
+        this.trendRecipient = newTrendRecipient;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Amount = " + trendAmount + "\n" + "Date = " + trendDate  + "\n" + "Recipient = " + trendRecipient);
+    }
     
 }
--- src/lab6se/MonthlyInvoiceDisplay.java
+++ src/lab6se/MonthlyInvoiceDisplay.java
@@ -5,10 +5,29 @@
  */
 package lab6se;
 
+import java.util.Date;
+
 /**
  *
  * @author 1301147
  */
-public class MonthlyInvoiceDisplay {
+public class MonthlyInvoiceDisplay implements Observer {
+    double monthlyAmount;
+    Date monthlyDate;
+    String monthlyRecipient;
+    private Subject monthlyData;
+    
+    @Override
+    public void update(double newMonthlyAmount, Date newMonthlyDate, String newMonthlyRecipient) {
+         this.monthlyAmount = newMonthlyAmount;
+        this.monthlyDate = newMonthlyDate;
+        this.monthlyRecipient = newMonthlyRecipient;
+        display();
+    }
+
+    @Override
+    public void display() {
+       System.out.println("Amount = " + monthlyAmount + "\n" + "Date = " + monthlyDate  + "\n" + "Recipient = " + monthlyRecipient);
+    }
     
 }
